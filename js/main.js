ac98bb48e8717a6edb744d3c74a3458b7964e8c6
Johna Paolino <johna@generalassemb.ly>
2016-12-29 20:41:50 -0500

    working link highlight

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const all = {
+  { name: '', description: '', image: '', url: '', tags: '' },
+  { name: '', description: '', image: '', url: '', tags: '' },
+  { name: '', description: '', image: '', url: '', tags: '' },
+  { name: '', description: '', image: '', url: '', tags: '' },
+}
