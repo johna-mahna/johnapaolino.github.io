597311a74ee2804ee8be8c74ae37c53c4faa1b5d
Johna Paolino <johna@generalassemb.ly>
2016-12-29 21:53:28 -0500

    mobile clean up

---
+++ js/highlight.js
@@ -0,0 +1,21 @@
+const triggers = document.querySelectorAll('.trigger');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink(){
+  // console.log("Highlightt!!");
+  const linkCoords = this.getBoundingClientRect();
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  };
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+triggers.forEach(tab => tab.addEventListener('mouseenter', highlightLink));
--- js/main.js
+++
@@ -1,6 +0,0 @@
-const all = {
-  { name: '', description: '', image: '', url: '', tags: '' },
-  { name: '', description: '', image: '', url: '', tags: '' },
-  { name: '', description: '', image: '', url: '', tags: '' },
-  { name: '', description: '', image: '', url: '', tags: '' },
-}
