5ba50c0c0e268a4f132026111ed4ef5f3e895e94
johnapaolino <johna.paolino@gmail.com>
2018-08-22 21:27:32 -0400

    major cleanup and reorg

--- js/tabs.js
+++
@@ -1,8 +0,0 @@
-const tabs = document.querySelectorAll('.tab');
-
-function updateActive(){
-  triggers.forEach(tab => tab.classList.remove('is-tab-selected'));
-  this.classList.add('is-tab-selected');
-}
-
-tabs.forEach(tab => tab.addEventListener('mousedown', updateActive));
